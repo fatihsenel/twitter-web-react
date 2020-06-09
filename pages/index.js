c6e2138de75383014bc1375ae63e9927361fbf76
fatihsenel <fatih_senel@msn.com>
2020-06-09 13:59:10 +0300

    initial

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Button from "../components/button";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Next JS</h1>
+      <Button>Merhaba</Button>
+    </div>
+  );
+}
+
+export default HomePage;
