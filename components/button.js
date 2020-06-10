370ba3f69e1cab219005fddbd7e50ab85c8d69aa
fatihsenel <fatih_senel@msn.com>
2020-06-10 13:25:22 +0300

    navigation completed

--- components/button.js
+++ components/button.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import styles from './button.module.css';
+import React from 'react'
+import styles from './button.module.css'
+import cn from 'classnames'
 
-function Button ({children}){
-    return <button type="button" className={styles.button}>{children}</button>
+function Button({ children, className, ...props }) {
+  return (
+    <button type="button" className={cn(styles.button, className)} {...props}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
