c2bcfdf57b6c602cc4880f72521e0d99f5eef123
fatihsenel <fatih_senel@msn.com>
2020-06-10 19:42:30 +0300

    layout ayarlandı

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
-import React from "react";
-
-import Button from "../components/button";
+import React from 'react'
+import CONST from '../constants'
+import Layout from '../components/layout'
+import useWindowSize from '../hooks/useWindowSize'
+import Sidebar from '../components/col-sidebar'
+import Main from '../components/col-main'
+import Extra from '../components/col-extra'
 
 function HomePage() {
+  const size = useWindowSize()
+
   return (
-    <div>
-      <h1>Welcome to Next JS</h1>
-      <Button>Merhaba</Button>
-    </div>
-  );
+    <Layout>
+      <Sidebar flat={size.width < CONST.DESKTOP_SIZE}>sidebar</Sidebar>
+      <Main>{JSON.stringify(size)}</Main>
+      {size.width > CONST.TABLET_SIZE && <Extra>extra</Extra>}
+    </Layout>
+  )
 }
 
-export default HomePage;
+export default HomePage
